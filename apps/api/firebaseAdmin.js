05aa695aa26fcb3fe8e68e6b28666be4638d395b
Seneth Jayashan <141265374+Seneth-Jayashan@users.noreply.github.com>
2025-10-11 03:34:08 +0530

    admin sdk error fixed

--- apps/api/firebaseAdmin.js
+++ apps/api/firebaseAdmin.js
@@ -1,11 +1,10 @@
 require('dotenv').config()
-const admin = require('firebase-admin')
+import admin from 'firebase-admin';
 
-// Convert the literal \n in private_key to actual newlines
-const serviceAccountJson = process.env.FIREBASE_SERVICE_ACCOUNT_JSON.replace(/\\n/g, '\n')
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
 
 admin.initializeApp({
-  credential: admin.credential.cert(JSON.parse(serviceAccountJson))
-})
+  credential: admin.credential.cert(serviceAccount),
+});
 
 module.exports = admin
